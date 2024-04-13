13f71f8bbf6b568f60e1142dec1ac0d33e302ffb
anton <tonyjkofficial@gmail.com>
2024-04-13 20:14:21 +0530

    New

--- src/test/java/org/Testngpro/MainClass.java
+++ src/test/java/org/Testngpro/MainClass.java
@@ -8,61 +8,87 @@ public class MainClass extends PageClass {
     PageClass pg;
 
     @BeforeSuite
-    public void (){
+    public void beforeSuite() {
 
     }
+
     @BeforeClass
-    public void (){
+    public void beforeClass() {
         long start = System.currentTimeMillis();
         System.out.println(start);
 
     }
+
     @BeforeTest
-    public void (){
+    public void beforeTest() {
 
     }
+
     @BeforeGroups
-    public void (){
+    public void beforeGroups() {
 
     }
+
     @BeforeMethod
-    public void startTime (){
+    public void beforeMethod() {
         long start = System.currentTimeMillis();
         System.out.println(start);
 
 
     }
+
     @Test
-        public void validateOrderNumber() throws Exception{
-            pg = new PageClass();
-            pg.browserLaunch();
-            pg.enterUserName();
-            pg.enterPassword();
-            pg.clickLogin();
-            pg.selectLocation();
-            pg.seclectHotel();
-            pg.selectRoom();
-            pg.noofAdults();
-            pg.noofChild();
-            pg.clickSearch();
-            pg.rdButton();
-            pg.nextContinue();
-            pg.fstName();
-            pg.lstName();
-            pg.fillAddress();
-            pg.cardNO();
-            pg.cardType();
-            pg.expcard();
-            pg.expYr();
-            pg.cardCvv();
-            pg.bookHere();
-            pg.orderNow();
+    public void validateOrderNumber() throws Exception {
+        pg = new PageClass();
+        pg.browserLaunch();
+        pg.enterUserName();
+        pg.enterPassword();
+        pg.clickLogin();
+        pg.selectLocation();
+        pg.seclectHotel();
+        pg.selectRoom();
+        pg.noofAdults();
+        pg.noofChild();
+        pg.clickSearch();
+        pg.rdButton();
+        pg.nextContinue();
+        pg.fstName();
+        pg.lstName();
+        pg.fillAddress();
+        pg.cardNO();
+        pg.cardType();
+        pg.expcard();
+        pg.expYr();
+        pg.cardCvv();
+        pg.bookHere();
+        pg.orderNow();
 
     }
+
     @AfterMethod
-    public void endTime (){
+    public void afterMethod() {
         long end = System.currentTimeMillis();
         System.out.println(end);
+
+    }
+
+    @AfterGroups
+    public void afterGroups() {
+
+    }
+
+    @AfterTest
+    public void afterTest() {
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+
+    }
+
+    @AfterSuite
+    public void afterSuite() {
 
     }
 
